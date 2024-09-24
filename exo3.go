e1928d445a37af4e984a85c2252954a0dfd236f0
NicolasDISEP <nicolas.nd40@gmail.com>
2024-09-24 16:24:20 +0200

    fix exo whitout struct

--- exo3.go
+++ exo3.go
@@ -1,26 +1,26 @@
 package exam
 
-type Interval struct {
-	Debut int
-	Fin   int
-}
+import "sort"
 
-func Ft_non_overlap(intervals []Interval) int {
-	for i := 0; i < len(intervals); i++ {
-		for j := i + 1; j < len(intervals); j++ {
-			if intervals[i].Fin > intervals[j].Fin {
-				intervals[i], intervals[j] = intervals[j], intervals[i]
-			}
-		}
-	}
+func Ft_non_overlap(intervals [][]int) int {
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][1] < intervals[j][1]
+	})
+
 	count := 0
-	FindernierInterval := intervals[0].Fin
+	if len(intervals) == 0 {
+		return count
+	}
+
+	endLastInterval := intervals[0][1]
+
 	for i := 1; i < len(intervals); i++ {
-		ActuInterval := intervals[i]
-		if ActuInterval.Debut < FindernierInterval {
+		currentInterval := intervals[i]
+
+		if currentInterval[0] < endLastInterval {
 			count++
 		} else {
-			FindernierInterval = ActuInterval.Fin
+			endLastInterval = currentInterval[1]
 		}
 	}
 
